aacd877f63056df71c1a4b38b021743823d7b483
ChloeGwon <chgmlk@gmail.com>
2022-11-15 14:25:49 -0800

    frontend-admin

--- GoBorrow_Frontend/src/components/admin/admin.jsx
+++ GoBorrow_Frontend/src/components/admin/admin.jsx
@@ -4,7 +4,7 @@ import {allUsers} from '../../services/allUsers'
 import DataTable from 'react-data-table-component'
 import { Link, useNavigate } from 'react-router-dom';
 import ProductDetail from '../productDetail/productDetail';
-import EditProduct from './editProduct';
+import EditProduct from './editProducts';
 
 
 const Admin = ({navigation}) => {
@@ -17,8 +17,8 @@ const Admin = ({navigation}) => {
     }
 
     const componentChange = (username) => {
-        <Link to={`/editproduct/${username}`}></Link>
-        navigate(`/editproduct/${username}`, {replace:true})
+        <Link to={`/productlists/${username}`}></Link>
+        navigate(`/productlists/${username}`, {replace:true})
     }
 
     const columns = [
@@ -45,12 +45,12 @@ const Admin = ({navigation}) => {
             right: true
         },
         {
-            name: "Actions",
+            name: "See All Products",
             button: true,
             cell: (row) => (
                 <button
                     className="btn btn-outline btn-xs"
-                    onClick={()=>componentChange(row.username)}> Edit
+                    onClick={()=>componentChange(row.username)}> View
                 </button>
             )
         }
